bf1763c68f564ac64ea9ba89c1a40d9a958c6d6a
Manojkumar144 <manojsantha1442000@gmail.com>
2024-04-02 11:39:16 +0530

    fixed database connection issue

--- util/database.js
+++ util/database.js
@@ -2,8 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD,{
     dialect: process.env.DATABASE_DIALECT,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT
+    host: process.env.DATABASE_HOST
 })
 
 module.exports = sequelize;
